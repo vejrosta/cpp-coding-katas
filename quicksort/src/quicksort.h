150f19dd58b3f96cc717cf65037fccfdc28caca5
Jiri Vejrosta <vejrosta.jiri@gmail.com>
2024-07-07 11:33:01 +0200

    clean-up

--- quicksort/src/quicksort.h
+++ quicksort/src/quicksort.h
@@ -9,22 +9,22 @@ int partition(std::vector<int>& arr, int low, int high) {
 	int pivot{arr[high]};
 	int i{(low - 1)};
 
-	for (int j{low}; j <= high - 1; j++) {
+	for (int j{low}; j < high; ++j) {
 		if (arr[j] < pivot) {
-			i++;
+			++i;
 			std::swap(arr[i], arr[j]);
 		}
 	}
 	std::swap(arr[i + 1], arr[high]);
-	return (i + 1);
+	return i + 1;
 }
 
 void quicksort(std::vector<int>& arr, int low, int high) {
 	if (low < high) {
-		int pi{partition(arr, low, high)};
+		int pivot{partition(arr, low, high)};
 
-		quicksort(arr, low, pi - 1);
-		quicksort(arr, pi + 1, high);
+		quicksort(arr, low, pivot - 1);
+		quicksort(arr, pivot + 1, high);
 	}
 }
 
