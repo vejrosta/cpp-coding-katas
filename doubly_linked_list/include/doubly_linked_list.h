dafe0d75b52532654e60b6ce35abc5ea2b156f09
Jiri Vejrosta <vejrosta.jiri@gmail.com>
2024-07-08 11:57:19 +0200

    Initial commit of doubly linked list implementation

---
+++ doubly_linked_list/include/doubly_linked_list.h
@@ -0,0 +1,24 @@
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+struct Node {
+  int data{};
+  Node *prev{nullptr};
+  Node *next{nullptr};
+};
+
+class DoublyLinkedList {
+public:
+  ~DoublyLinkedList();
+
+  Node *getHead() const;
+  Node *getTail() const;
+
+  bool empty() const;
+
+private:
+  Node *head{nullptr};
+  Node *tail{nullptr};
+};
+
+#endif // DOUBLY_LINKED_LIST_H
---
+++ doubly_linked_list/tests/doubly_linked_list_test.cpp
@@ -0,0 +1,7 @@
+#include "doubly_linked_list.h"
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
